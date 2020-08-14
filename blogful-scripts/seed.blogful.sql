569b19b238d0ff5304c6d7d2ded8a914ddbef0fb
Marli Renee <marlirenee@gmail.com>
2020-08-14 15:24:20 -0700

    commit for help with unexpected identifier error

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+
+VALUES
+    ('The Eleventh Hour 1', now() - '21 days'::INTERVAL, 'yada yada'),
+    ('The Beast Below', now() - '21 days'::INTERVAL, 'uada uada'),
+    ('Victory of the Daleks', now() - '21 days'::INTERVAL,'iada iada'),
+    ('The Time of Angels', now() - '21 days'::INTERVAL,'oada oada'),
+    ('Flesh and Stone', now() - '21 days'::INTERVAL, 'pada pada'),
+    ('Vampires of Venice', now() - '9 days'::INTERVAL, 'aada aada'),
+    ('Amys Choice', now() - '9 days'::INTERVAL, 'sada sada'),
+    ('The Hungry Earth', now() - '9 days'::INTERVAL, 'dada dada'),
+    ('Cold Blood', now() - '9 days'::INTERVAL, 'fada fada'),
+    ('Vincent and the Doctor', now() - '9 days'::INTERVAL, 'gada gada'),
+    ('The Lodger', now() - '7 days'::INTERVAL, 'hada hada'),
+    ('The Pandorica Opens', now() - '7 days'::INTERVAL,'jada jada'),
+    ('The Big Bang', now() - '5 days'::INTERVAL,'kada kada'),
+    ('Silence in the Libary', now() - '5 days'::INTERVAL,'lada lada'),
+    ('The Unicorn and the Wasp', now() - '5 days'::INTERVAL,'zada zada'),
+    ('The Impossible Planet', now() - '5 days'::INTERVAL,'xada xada'),
+    ('Blink', now() - '4 days'::INTERVAL,'cada cada'),
+    ('Forest of the Dead', now() - '4 days'::INTERVAL,'vada vada'),
+;
+
+
